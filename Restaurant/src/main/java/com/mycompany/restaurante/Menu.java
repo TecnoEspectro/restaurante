47e68e9d6b1ebdb41ea52593fc0b137751efe195
Tecno Espectro <TecnoEspectro15@hotmail.com>
2024-08-27 07:13:27 -0500

    hoy

--- Restaurant/src/main/java/com/mycompany/restaurante/Menu.java
+++ Restaurant/src/main/java/com/mycompany/restaurante/Menu.java
@@ -17,7 +17,7 @@ public class Menu {
     String dias[]=new String[]{"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"};
     int suma = 0;
     int mas = 0;
-    int menos = 0;
+    int menos = 100;
     int totalMas = 0;
     int totalMenos = 0;
     String masDia;
@@ -27,6 +27,8 @@ public class Menu {
     String totDia;
     String menDia;
     int sumaFinal = 0;
+    int prom = 0;
+    int promFinal = 0;
     
     public void crearMenu(int cantity){
     
@@ -70,35 +72,39 @@ public class Menu {
     
         System.out.println("Mostrar platos vendidos individualmente ");
         System.out.println("Plato 1");
+        sumaFinal=0;
         for(int c=0; c<platos.length; c++){
         suma=0;
         mas=0;
-        menos=0;
+        menos=100;
             for(int f=0; f<6; f++){
-                if(mas<datos[f][c]){mas=datos[f][c];}
-                if(menos>datos[f][c]){menos=datos[f][c];}
+                
                 if(mas<datos[f][c]){masDia=dias[f];}
                 if(menos>datos[f][c]){menosDia=dias[f];}
                 if(mas<datos[f][c]){sumaTotal=datos[f][c];}
                 if(menos>datos[f][c]){restaTotal=datos[f][c];}
                 if(mas<datos[f][c]){totDia=dias[f];}
                 if(menos>datos[f][c]){menDia=dias[f];}
+                if(mas<datos[f][c]){mas=datos[f][c];}
+                if(menos>datos[f][c]){menos=datos[f][c];}
                 suma+=datos[f][c];
                 
             }
-            
+            prom = suma/6;
             totalMas+=sumaTotal;
             totalMenos+=restaTotal;
             sumaFinal+=suma;
             
                  JOptionPane.showMessageDialog(null, "Ventas totales del plato " + platos[c] + " en la semana es de " + suma);
                  JOptionPane.showMessageDialog(null, "La mayor venta del plato fue de "+ mas + " en el dia " + masDia + " y la menor venta del plato fue de " + menos + " en el dia " + menosDia);
+                 JOptionPane.showMessageDialog(null, "Promedio de ventas del plato en la semana fue de "+ prom);
                  
         }
-        
+        promFinal = sumaFinal/6;
         JOptionPane.showMessageDialog(null, "La mayor venta de los platos fue de "+ sumaTotal + " en el dia " + totDia + " y la menor venta de los platos fue de " + restaTotal + " en el dia " + menDia);
         JOptionPane.showMessageDialog(null, "Sumando los dias de mayor venta de los platos, la venta maxima fue "+ totalMas + " y sumando los dias de menor venta, las ventas minimas fueron " + totalMenos);
         JOptionPane.showMessageDialog(null, "La venta total entre todos los platos fue de "+ sumaFinal);
+        JOptionPane.showMessageDialog(null, "Promedio por ventas totales "+ promFinal);
         
     }
     
